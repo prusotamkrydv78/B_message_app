ef0e72c5674681dca7ebb21114acb7209064ad27
Prusotam <prusotamkumaryadav78@gmail.com>
2025-09-06 16:46:09 +0545

    server checking

--- src/app.js
+++ src/app.js
@@ -22,9 +22,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', timestamp: new Date().toISOString() });
-});
+app.get('/', (req, res) => res.send("Backend is live!"));
+
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', usersRoutes);
